efef579a11ca38a5aecb8288a374389e6692cf86
Abhishek9860 <104040276+Abhishek9860@users.noreply.github.com>
2024-03-04 10:34:18 +0530

    Added Navbar and Offer Header

--- src/components/Home/Navbar/Navbar.js
+++ src/components/Home/Navbar/Navbar.js
@@ -1,17 +1,42 @@
-import React from 'react'
-import './Navbar.css';
+import React from "react";
+import "./Navbar.css";
+
 export default function Navbar() {
   return (
-    <div className='navbar'>
+    <>
+    <div className="offer"><p>Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!  <a className="shopNow" href="/"><strong>Shop Now</strong></a></p></div>
     <nav>
-      <div className='logo'>ShopNest</div>
-      <ul>
-        <li><a href="">Home</a></li>
-        <li><a href="">About</a></li>
-        <li><a href="">Contact</a></li>
-        <li><a href="">Sigh Up</a></li>
-      </ul>
+      <div className="navbar">
+        <div className="logo">ShopNest</div>
+        <div className="tabs">
+          <ul>
+            <li className="nav_tab">
+              <a href="/">Home</a>
+            </li>
+            <li className="nav_tab">
+              <a href="/">About</a>
+            </li>
+            <li className="nav_tab">
+              <a href="/">Contact</a>
+            </li>
+            <li className="nav_tab">
+              <a href="/">Sign Up</a>
+            </li>
+          </ul>
+        </div>
+        <form>
+          <input
+            type="search"
+            placeholder="what are you looking for?"
+            aria-label="Search"
+          />
+          <button type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
+          <button type="button"><i class="fa-regular fa-heart"></i></button>
+          <button type="button"><i class="fa-solid fa-cart-shopping"></i></button>
+        </form>
+      </div>
     </nav>
-    </div>
-  )
+    <hr />
+    </>
+  );
 }
