3b5619ca547f610f1aaaf72b99246a0970275060
Abhishek9860 <104040276+Abhishek9860@users.noreply.github.com>
2024-03-12 04:48:28 +0530

    added Aside section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Navbar from "./components/Home/Navbar/Navbar";
+import Aside from "./components/Aside/Aside";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Aside />
     </>
   );
 }
